17087997a68e60da0c4f13172351d6655b23b566
okan.toper <okan.toper@foriba.com>
2019-09-14 21:58:44 +0300

    component  1 finish

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -3,20 +3,31 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
     selector: 'events-thumbnail',
     template: `
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: $/{{event.price}}</div>
-        <div>
-            <span>Location : {{event.location.adress}} - {{event.location.city}} - {{event.location.country}}</span>
+        <h2 [style.color]="events.name === 'Angular Connect' ? '#ecf0f1' : '#3498db'">{{events?.name}}</h2>
+        <div>Date: {{events?.date}}</div>
+        <div [ngClass]="{green: events?.time === '8:00 am', bold:events?.time === '8:00 am'}"
+            [ngSwitch]="events?.time">Time: {{events?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
         </div>
-    `
-    
+        <div>Price: $/{{events?.price}}</div>
+        <div *ngIf="events?.location">
+            <span>Location : {{events?.location?.adress}} - {{events?.location?.city}} - {{events?.location?.country}}</span>
+        </div>
+        <div *ngIf="events.onlineURL">Online URL: {{events?.onlineURL}}</div>
+    `,
+    styles: [`
+        h2 {color:blue}
+        .green {color:green !important}
+        .bold { font-weight:bolder !important }
+
+    `]
 })
 export class EventsThumbnailComponent {
-   @Input() event: any;
-   someProperty:any = "Some Property"
-   logFoo(){
-       console.log('Console Foo')
-   }
+    @Input() events: any;
+    someProperty: any = "Some Property"
+    logFoo() {
+        console.log(this.someProperty)
+    }
 }
