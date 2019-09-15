e5034330166cbbe5cdf19163873187914b570db1
okan.toper <okan.toper@foriba.com>
2019-09-15 08:30:05 +0300

    services branch complited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/events-thumbnail.component';
 import { Navbar } from './nav/navbar.component';
+import { EventService } from './shared/event.service';
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,7 @@ import { Navbar } from './nav/navbar.component';
   imports: [
     BrowserModule,
   ],
-  providers: [],
+  providers: [EventService,ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
